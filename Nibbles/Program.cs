ea71373c0ab6eea89627978791507caa3a9c0460
RosiAvramova <rosi.avramova1@gmail.com>
2022-06-05 22:43:38 +0300

    Add project files.

---
+++ Nibbles/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Rosi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int testCases = int.Parse(Console.ReadLine());
+            for (int i = 0; i < testCases; i++)
+            {
+                int bytes = int.Parse(Console.ReadLine());
+                if (bytes % 4 == 0)
+                {
+                    Console.WriteLine("Good");
+                }
+                else
+                {
+                    Console.WriteLine("Not Good");
+                }
+            }
+
+
+
+
+
+
+        }
+    }
+}
